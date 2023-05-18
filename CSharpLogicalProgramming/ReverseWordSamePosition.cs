3f15b29b6131769b4a2a57c2f558ef8ed7d906a4
unknown <amoljadhav>
2023-05-18 10:31:13 +0530

    [Amol] Reverse Word at Same Position

---
+++ CSharpLogicalProgramming/ReverseWordSamePosition.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCSharpProgramsDemo281Batch_
+{
+    public class ReverseWordSamePosition
+    {
+        public static void ReverseWord(string word)
+        {
+            string reverse = "";
+            foreach (char letter in word)
+            {
+                reverse = letter + reverse;
+
+            }
+            Console.WriteLine(reverse);
+        }
+
+        public static void ReverseEachWordInSamePosition(string word)
+        {
+            string sentance = "";
+            string[] words = word.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+
+            {
+                string reverse = "";
+                foreach (char letter in words[i])
+                {
+                    reverse = letter + reverse;
+                }
+                sentance = sentance + reverse + " ";
+            }
+            Console.WriteLine(sentance);
+        }
+    }
+}
